a2c1c5654cb9713db6f8bf3734fc3eeee9f9bc43
Akhil <akhil.palepally1@gmail.com>
2026-07-28 12:37:43 +0530

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        unordered_map<int,int> hashbrown;
+        for (int i = 0; i < nums.size(); i++){
+            hashbrown[nums[i]] = i;
+        }
+        for (int i = 0; i<nums.size(); i++){
+            for(int j = 0; j < nums.size(); j++){
+                if(i != j){
+                    int val1 = nums[i];
+                    int val2 = nums[j];
+                    if(hashbrown.count(-val1-val2) && hashbrown[-val1-val2] != i && hashbrown[-val1-val2] != j){
+                        vector<int> temp = {val1, val2, -val1-val2};
+                        sort(temp.begin(), temp.end());
+                        auto it = find(result.begin(), result.end(), temp);
+                        if(it == result.end()){
+                            result.push_back(temp);
+                        }
+                    }
+                }
+
+
+            }
+            
+        }
+        return result;
+    }
+};
